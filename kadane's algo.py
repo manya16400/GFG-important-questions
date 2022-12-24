f30a483486ab5da29737c6940b027c3770c5fc9a
Manya Rampuria <60806637+manya16400@users.noreply.github.com>
2022-12-23 23:53:12 -0500

    Update kadane's algo.py

--- kadane's algo.py
+++ kadane's algo.py
@@ -1,3 +1,19 @@
+"""
+
+Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.
+
+Input:
+N = 5
+Arr[] = {1,2,3,-2,5}
+Output:
+9
+Explanation:
+Max subarray sum is 9
+of elements (1, 2, 3, -2, 5) which 
+is a contiguous subarray.
+
+"""
+
 #Function to find the sum of contiguous subarray with maximum sum.
 def maxSubArraySum(self,arr,N):
     ##Your code here
