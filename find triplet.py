c6548f11cba323c84830f3a48ac4681178d4aa09
Manya Rampuria <60806637+manya16400@users.noreply.github.com>
2022-12-23 23:52:22 -0500

    created

--- find triplet.py
+++ find triplet.py
@@ -1,3 +1,15 @@
+"""
+Given an array of distinct integers. The task is to count all the triplets such that sum of two elements equals the third element.
+
+Input: 
+N = 4 
+arr[] = {1, 5, 3, 2}
+Output: 2 
+Explanation: There are 2 triplets:
+ 1 + 2 = 3 and 3 +2 = 5
+ 
+ """
+
 class Solution:
 	def countTriplet(self, arr, n):
 		c = 0
