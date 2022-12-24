d0e6da88b8423c02a513dc1a15deadf08557cee8
Manya Rampuria <60806637+manya16400@users.noreply.github.com>
2022-12-23 23:51:17 -0500

    created

---
+++ missing number in arr.py
@@ -0,0 +1,13 @@
+def MissingNumber(self,array,n):
+  return n*(n+1)//2 - sum(array)
+
+
+"""
+Input: arr[] = {1, 2, 4, 6, 3, 7, 8}, N = 7
+Output: 5
+Explanation: The missing number between 1 to 8 is 5
+
+Input: arr[] = {1, 2, 3, 5}, N = 5
+Output: 4
+Explanation: The missing number between 1 to 5 is 4
+"""
