663e97c8fbd4285d1d8849ff2cf365804d8f9ee9
Manya Rampuria <60806637+manya16400@users.noreply.github.com>
2022-12-26 21:15:20 -0500

    .

--- 2. count triplet.py
+++ 2. count triplet.py
@@ -10,17 +10,16 @@ Explanation: There are 2 triplets:
  
  """
 
-class Solution:
-	def countTriplet(self, arr, n):
-		c = 0
-		d = {}
+def countTriplet(self, arr, n):
+	c = 0
+	d = {}
     
     for i in range(n):
-        d[arr[i]]=i
+	d[arr[i]]=i
 
     for i in range(n-1):
-        for j in range(i+1,n):
-             if arr[i]+arr[j] in d:
-                     c+=1
+	for j in range(i+1,n):
+	     if arr[i]+arr[j] in d:
+		     c+=1
 
     return c
